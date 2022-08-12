ce07dae07840b7a2652d91b9befb440f8cc16340
Andrew Galvin <andrewggalvin@gmail.com>
2022-08-12 19:28:58 -0400

    Added NxN and AI vs AI

--- board/game.py
+++ board/game.py
@@ -1,25 +1,32 @@
 import sys
 import argparse
+from random import randint
 from minimax import search_for_best_move
 import os
+import numpy as np
 
 
 def cls():
-    os.system('cls' if os.name=='nt' else 'clear')
+    os.system("cls" if os.name == "nt" else "clear")
+
 
 def printBoard(board):
     for row in board:
         print(row)
 
+
 def findIndex(move):
     move -= 1
     return (move // 3, move % 3)
 
+
 def pickSpace():
-    space = int(input("\nSelect a square: "))
-    if not 1 <= space <= 9:
-        raise ValueError
-    return space
+    row = int(input("Select a row: "))
+    column = int(input("Select a column: "))
+    # if not 1 <= space <= 9:
+    #     raise ValueError
+    return row, column
+
 
 def markMove(move, x, board):
     i, j = move
@@ -28,69 +35,119 @@ def markMove(move, x, board):
     else:
         raise ValueError
 
+
 def checkDraw(board):
     for row in board:
         for ele in row:
             if ele == "_":
                 return False
+    print("\nThe game is a draw")
     printBoard(board)
-    print("The game is a draw")
     return True
 
+    # def checkWin(board):
+    # winner = None
+    # for i in range(3):
+    #     # horizontal win
+    #     if board[i][0] == board[i][1] == board[i][2] and board[i][0] != "_":
+    #         winner = board[i][0]
+    #         break
+    #     # vertical win
+    #     if board[0][i] == board[1][i] == board[2][i] and board[0][i] != "_":
+    #         winner = board[0][i]
+    #         break
+    # # diagonal win
+    # if board[1][1] != "_":
+    #     if (
+    #         board[0][0] == board[1][1] == board[2][2]
+    #         or board[0][2] == board[1][1] == board[2][0]
+    #     ):
+    #         winner = board[1][1]
+    # if winner is not None:
+    #     print("\n\n\n")
+    #     printBoard(board)
+    #     print(winner + " is the winner of the game!")
+    #     return True
+    # return False
+
+
+def checkRows(board):
+    for row in board:
+        if "_" not in row:
+            if len(set(row)) == 1:
+                return row[0]
+    return 0
+
+
+def checkDiagonals(board):
+    if len(set([board[i][i] for i in range(len(board))])) == 1:
+        if board[0][0] != "_":
+            return board[0][0]
+    if len(set([board[i][len(board) - i - 1] for i in range(len(board))])) == 1:
+        if board[0][len(board) - 1] != "_":
+            return board[0][len(board) - 1]
+    return 0
+
+
 def checkWin(board):
-    winner = None
-    for i in range(3):
-        #horizontal win
-        if board[i][0] == board[i][1] == board[i][2] and board[i][0] != "_":
-            winner = board[i][0]
-            break
-        #vertical win
-        if board[0][i] == board[1][i] == board[2][i] and board[0][i] != "_":
-            winner = board[0][i]
-            break
-    #diagonal win
-    if board[1][1] != "_":
-        if (board[0][0] == board[1][1] == board[2][2] or board[0][2] == board[1][1] == board[2][0]):
-            winner = board[1][1]
-    if winner is not None:
-        print("\n\n\n")
-        printBoard(board)
-        print(winner + " is the winner of the game!")
-        return True
-    return False
+    # transposition to check rows, then columns
+    for newBoard in [board, np.transpose(board)]:
+        result = checkRows(newBoard)
+        if result:
+            return result
+    return checkDiagonals(board)
+
+
+def create_board(n):
+    return [["_" for _ in range(n)] for _n in range(n)]
 
 
 def main():
-    sample_board = [[1,2,3],[4,5,6],[7,8,9]]
 
-    
+    print("Please enter a NxN game you would like to play:\n")
+    N = int(input("N: "))
+    # print(checkWin(create_board(N)))
 
-    board = [["_" for _ in range(3)] for _ in range(3)]
+    board = create_board(N)
     x = True
     goal = False
     err = False
+    count = 0
 
     while not goal:
 
         if x == False:
             # have ai play
-            move = search_for_best_move(board)
+            print(f"\nCurrent Board [{count}]:")
+            printBoard(board)
+            move = search_for_best_move(board, N, "O", "X")
             markMove(move, x, board)
         else:
-            cls()
-            print("\n\nWelcome to Tic-Tac-Toe, below is a sample of where you can pick to play:")
-            printBoard(sample_board)
-            print("\n")
-            print("\nCurrent Board:")
+            # cls()
+            print(f"\nCurrent Board [{count}]:")
             printBoard(board)
-            try:
-                move = findIndex(pickSpace())
-                markMove(move, x, board)
-            except ValueError:
-                print("Must select an unoccupied square 1-9")
-                continue
-        goal = checkWin(board) or checkDraw(board)
+            # Uncomment out below code to play manually
+            # try:
+            #     move = pickSpace()
+            #     markMove(move, x, board)
+            # except ValueError:
+            #     print("Must select an unoccupied square 1-9")
+            #     continue
+
+            # BELOW CODE IS FOR AI TO PLAY AGAINST ITSELF
+            if count == 0:
+                move = randint(0, N - 1), randint(0, N - 1)
+            else:
+                move = search_for_best_move(board, N, "X", "O")
+            markMove(move, x, board)
+        goal = checkDraw(board)
+        winner = checkWin(board)
+        if winner != 0:
+            print(winner, "has won!")
+            printBoard(board)
+            goal = True
         x = not x
+        count += 1
 
 
 if __name__ == "__main__":
--- board/minimax.py
+++ board/minimax.py
@@ -1,135 +1,136 @@
-player, opponent = 'O', 'X'
+import numpy as np
 
-def can_play(board) :
-	for i in range(3) :
-		for j in range(3) :
-			if (board[i][j] == '_') :
-				return True
-	return False
 
-def check_if_someone_won(b) :
+def can_play(board, N):
+    for i in range(N):
+        for j in range(N):
+            if board[i][j] == "_":
+                return True
+    return False
 
-	# Checking for Rows for X or O victory.
-	for row in range(3) :	
-		if (b[row][0] == b[row][1] and b[row][1] == b[row][2]) :	
-			if (b[row][0] == player) :
-				return 10
-			elif (b[row][0] == opponent) :
-				return -10
 
-	# Checking for Columns for X or O victory.
-	for col in range(3) :
-	
-		if (b[0][col] == b[1][col] and b[1][col] == b[2][col]) :
-		
-			if (b[0][col] == player) :
-				return 10
-			elif (b[0][col] == opponent) :
-				return -10
+def checkRows(board):
+    for row in board:
+        if "_" not in row:
+            if len(set(row)) == 1:
+                return row[0]
+    return 0
 
-	# Checking for Diagonals for X or O victory.
-	if (b[0][0] == b[1][1] and b[1][1] == b[2][2]) :
-	
-		if (b[0][0] == player) :
-			return 10
-		elif (b[0][0] == opponent) :
-			return -10
 
-	if (b[0][2] == b[1][1] and b[1][1] == b[2][0]) :
-	
-		if (b[0][2] == player) :
-			return 10
-		elif (b[0][2] == opponent) :
-			return -10
+def checkDiagonals(board):
+    if len(set([board[i][i] for i in range(len(board))])) == 1:
+        if board[0][0] != "_":
+            return board[0][0]
+    if len(set([board[i][len(board) - i - 1] for i in range(len(board))])) == 1:
+        if board[0][len(board) - 1] != "_":
+            return board[0][len(board) - 1]
+    return 0
 
-	# Else if none of them have won then return 0
-	return 0
 
-def minimax(board, depth, isMax) :
-	score = check_if_someone_won(board)
+def check_if_someone_won(board, player, opponent):
+    for newBoard in [board, np.transpose(board)]:
+        result = checkRows(newBoard)
+        if result:
+            if result == player:
+                return 10
+            elif result == opponent:
+                return -10
+    diagonals = checkDiagonals(board)
+    if diagonals == player:
+        return 10
+    elif diagonals == opponent:
+        return -10
+    else:
+        return 0
 
-	# maximizer won
-	if (score == 10) :
-		return score
 
-	# minimizer won
-	if (score == -10) :
-		return score
+def minimax(board, depth, isMax, N, player, opponent):
+    score = check_if_someone_won(board, player, opponent)
+    # maximizer won
+    if score == 10:
+        return score
 
-	# tie
-	if (can_play(board) == False) :
-		return 0
+    # minimizer won
+    if score == -10:
+        return score
 
-	# maximizers move
-	if (isMax) :	
-		best = -1000
+    # tie
+    if can_play(board, N) == False:
+        return 0
 
-		# Traverse all cells
-		for i in range(3) :		
-			for j in range(3) :
-			
-				# Check if cell is empty
-				if (board[i][j]=='_') :
-				
-					# Make the move
-					board[i][j] = player
+    # maximizers move
+    if isMax:
+        best = -1000
 
-					# Call minimax recursively and choose
-					# the maximum value
-					best = max( best, minimax(board,
-											depth + 1,
-											not isMax) )
+        # Traverse all cells
+        for i in range(N):
+            for j in range(N):
 
-					# Undo the move
-					board[i][j] = '_'
-		return best
+                # Check if cell is empty
+                if board[i][j] == "_":
 
-	# minimizers move
-	else :
-		best = 1000
+                    # Make the move
+                    board[i][j] = player
 
-		# Traverse all cells
-		for i in range(3) :		
-			for j in range(3) :
-			
-				# Check if cell is empty
-				if (board[i][j] == '_') :
-				
-					# Make the move
-					board[i][j] = opponent
+                    # Call minimax recursively and choose
+                    # the maximum value
+                    best = max(
+                        best, minimax(board, depth + 1, not isMax, N, player, opponent)
+                    )
 
-					# Call minimax recursively and choose
-					# the minimum value
-					best = min(best, minimax(board, depth + 1, not isMax))
+                    # Undo the move
+                    board[i][j] = "_"
+        return best
 
-					# Undo the move
-					board[i][j] = '_'
-		return best
+    # minimizers move
+    else:
+        best = 1000
+
+        # Traverse all cells
+        for i in range(N):
+            for j in range(N):
+
+                # Check if cell is empty
+                if board[i][j] == "_":
+
+                    # Make the move
+                    board[i][j] = opponent
+
+                    # Call minimax recursively and choose
+                    # the minimum value
+                    best = min(
+                        best, minimax(board, depth + 1, not isMax, N, player, opponent)
+                    )
+
+                    # Undo the move
+                    board[i][j] = "_"
+        return best
+
 
 # This will return the best possible move for the player
-def search_for_best_move(board) :
-	bestVal = -1000
-	bestMove = (-1, -1)
+def search_for_best_move(board, N, p, o):
+    bestVal = -1000
+    bestMove = (-1, -1)
 
-	# check all of board
-	for i in range(3) :	
-		for j in range(3) :
-		
-			# Check if cell is empty
-			if (board[i][j] == '_') :
-			
-				# Make the move
-				board[i][j] = player
+    # check all of board
+    for i in range(N):
+        for j in range(N):
 
-				# compute evaluation function for this
-				# move.
-				moveVal = minimax(board, 0, False)
+            # Check if cell is empty
+            if board[i][j] == "_":
 
-				# Undo the move
-				board[i][j] = '_'
+                # Make the move
+                board[i][j] = p
 
-				if (moveVal > bestVal) :			
-					bestMove = (i, j)
-					bestVal = moveVal
+                # compute evaluation function for this
+                # move.
+                moveVal = minimax(board, 0, False, N, p, o)
 
-	return bestMove
+                # Undo the move
+                board[i][j] = "_"
+
+                if moveVal > bestVal:
+                    bestMove = (i, j)
+                    bestVal = moveVal
+
+    return bestMove
